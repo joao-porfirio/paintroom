6494550005436ed0239ffbbaa0ad9283fd746d00
Joao Porfirio <joaoprf54@gmail.com>
2022-05-03 15:24:57 -0300

    Upload arquivo central App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Formulario } from './components/Formulario';
+import { Header } from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Exibicao } from './components/Exibicao';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Header />
+				<Routes>
+					<Route exact path="/" element={<Formulario />} />
+					<Route exact path="/exibicao" element={<Exibicao />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
